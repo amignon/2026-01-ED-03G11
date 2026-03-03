fc2b46c0c0678aef1e0b7312257db1640af58d68
amignon <amignon@gmail.com>
2026-03-03 13:45:23 +0000

    Exemplo Pilha

---
+++ pilhas/Pilha.java
@@ -0,0 +1,62 @@
+public class Pilha {
+    // atributos
+    private static final int TAM_DEFAULT = 100;
+    private int topoPilha;
+    private int elementos[];
+    // construtores
+    public Pilha(int tamanho) {
+        this.elementos = new int[tamanho];
+        this.topoPilha = -1;
+    }
+
+    public Pilha() {
+        this(TAM_DEFAULT);
+    }
+    // métodos
+    public boolean isEmpty() {
+        return this.topoPilha == -1;
+    }
+
+    public boolean isFull() {
+        return topoPilha == elementos.length - 1;
+    }
+
+    public void push(int e) throws Exception {
+        if (!this.isFull()) {
+            topoPilha++;
+            this.elementos[topoPilha] = e;
+        }
+        else {
+            throw new Exception("Overflow - Estouro de pilha.");
+        }
+    }
+
+    public int pop() throws Exception {
+        if (!this.isEmpty()) {
+            int temp = this.elementos[topoPilha];
+            topoPilha--;
+            return temp;
+        }
+        else {
+            throw new Exception("Underflow - Esvaziamento de Pilha.");
+        }
+    }
+
+    public int topo() throws Exception {
+        if (!this.isEmpty()) {
+            return this.elementos[topoPilha];
+        }
+        else {
+            throw new Exception("Underflow - Esvaziamento de Pilha.");
+        }
+    }
+    
+    public int sizeElements() {
+        return topoPilha + 1;
+    }
+
+
+
+
+    
+}
