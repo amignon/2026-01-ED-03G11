f8af83c81b5388223ee46e34efb8d2afa3f4c422
amignon <amignon@gmail.com>
2026-05-12 12:30:39 +0000

    Exemplo Pacotes

---
+++ exemplo_pacotes/Programa.java
@@ -0,0 +1,8 @@
+
+
+public class Programa {
+    public static void main(String[] args) {
+        exemplo_pacotes.pacote_ed.Exemplo e = new Exemplo();
+        e.olaMundo();
+    }
+}
---
+++ exemplo_pacotes/pacote_ed/Exemplo.java
@@ -0,0 +1,7 @@
+package pacote_ed;
+
+public class Exemplo { 
+    public void olaMundo() {
+        System.out.println("Olá, Mundo!");
+    }
+}
